8dca4d8e5010c5e3f3c6d0fda6a4c80fe9ea1b2d
minh253 <doannhatminh253@gmail.com>
2023-06-09 10:37:25 +0700

    press k to repeatedly choose objects to track

--- scripts/main.py
+++ scripts/main.py
@@ -2,25 +2,37 @@
 # Author: MINH DOAN
 # Email: doannhatminh253@gmail.com
 
-
 import numpy as np
 import cv2
 import sys
+import serial
+
 
 # functions
-def CalculatePixelDistance(target: tuple[int, int], crosshair: tuple[int, int]) -> tuple[int, int]:
+def CalculatePixelDistance(
+    target: tuple[int, int], crosshair: tuple[int, int]
+) -> tuple[int, int]:
     return crosshair[0] - target[0], crosshair[1] - target[1]
 
 
-# init tracker
-tracker = cv2.legacy.TrackerKCF_create()
-
-# init video capture object
+# init video capture object and serial port
 camera_id = 2  # try 0, 1 or 2 if encounter errors
 if "linux" in sys.platform:
     cap = cv2.VideoCapture(camera_id, cv2.CAP_V4L)
+    arduino = serial.Serial(
+        port="/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_7583033343935150A092-if00",
+        baudrate=115200,
+        timeout=0,
+    )
 elif "win" in sys.platform:
     cap = cv2.VideoCapture(camera_id, cv2.CAP_DSHOW)
+    arduino = serial.Serial(
+        port="COM3",
+        baudrate=115200,
+        timeout=0,
+    )
+
+print("Initialization done!")
 
 # FRAME setting
 FRAME_WIDTH = 640
@@ -42,7 +54,7 @@ color_dict = {
     "blue": (255, 0, 0),
     "yellow": (0, 255, 255),
     "black": (0, 0, 0),
-    "white": (255, 255, 255)
+    "white": (255, 255, 255),
 }
 
 # main loop
@@ -51,24 +63,38 @@ while cap.isOpened():
     _, frame = cap.read()
 
     key = cv2.waitKey(5)
-    if key == ord('q'):
+    if key == ord("q"):
         break
-    elif key == ord('k'):
-        cv2.putText(frame,
-                    text="Select an object to track and then press SPACE to confirm!",
-                    org=(3, 15),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=0.5,
-                    color=color_dict["yellow"],
-                    thickness=1)
-        cv2.putText(frame,
-                    text="Press c to cancel!",
-                    org=(3, 30),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=0.5,
-                    color=color_dict["yellow"],
-                    thickness=1)
-        bbox = cv2.selectROI(windowName="Webcam Feed", img=frame, showCrosshair=True, fromCenter=False)
+    elif key == ord("k"):
+        # init tracker
+        tracker = cv2.legacy.TrackerKCF_create()
+
+        # init bounding box
+        bbox = (0, 0, 0, 0)  # x, y, w, h
+
+        # center of object
+        mid_x, mid_y = 0, 0
+        cv2.putText(
+            frame,
+            text="Select an object to track and then press SPACE to confirm!",
+            org=(3, 15),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=0.5,
+            color=color_dict["yellow"],
+            thickness=1,
+        )
+        cv2.putText(
+            frame,
+            text="Press c to cancel!",
+            org=(3, 30),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=0.5,
+            color=color_dict["yellow"],
+            thickness=1,
+        )
+        bbox = cv2.selectROI(
+            windowName="Webcam Feed", img=frame, showCrosshair=True, fromCenter=False
+        )
         cv2.destroyWindow("Webcam Feed")
 
     if bbox != (0, 0, 0, 0):
@@ -76,50 +102,96 @@ while cap.isOpened():
         ok, bbox = tracker.update(frame)
         bbox = tuple(map(int, bbox))
         if ok:
+            # calculate errors
             mid_x = bbox[0] + int(bbox[2] / 2)
             mid_y = bbox[1] + int(bbox[3] / 2)
-            cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), color_dict["red"], 3)
-            cv2.line(frame, (mid_x, bbox[1] + bbox[3] // 3), (mid_x, bbox[1] + bbox[3] - bbox[3] // 3),
-                     color_dict["blue"], 2)  # vertical
-            cv2.line(frame, (bbox[0] + bbox[2] // 3, mid_y), (bbox[0] + bbox[2] - bbox[2] // 3, mid_y),
-                     color_dict["blue"], 2)  # horizontal
             x_diff, y_diff = CalculatePixelDistance((mid_x, mid_y), mid_frame)
-            cv2.putText(frame,
-                        text=f"Horizontal Error: {x_diff}",
-                        org=(3, FRAME_HEIGHT - 3),
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                        fontScale=0.75,
-                        color=color_dict["blue"],
-                        thickness=2)
-            cv2.putText(frame,
-                        text=f"Vertical Error: {y_diff}",
-                        org=(3, FRAME_HEIGHT - 18),
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                        fontScale=0.75,
-                        color=color_dict["blue"],
-                        thickness=2)
+
+            # transmit error to arduino
+            arduino.write(f"{x_diff}".encode("utf-8"))
+
+            # display bounding box
+            cv2.rectangle(
+                frame,
+                (bbox[0], bbox[1]),
+                (bbox[0] + bbox[2], bbox[1] + bbox[3]),
+                color_dict["red"],
+                3,
+            )
+            cv2.line(
+                frame,
+                (mid_x, bbox[1] + bbox[3] // 3),
+                (mid_x, bbox[1] + bbox[3] - bbox[3] // 3),
+                color_dict["blue"],
+                2,
+            )  # vertical
+            cv2.line(
+                frame,
+                (bbox[0] + bbox[2] // 3, mid_y),
+                (bbox[0] + bbox[2] - bbox[2] // 3, mid_y),
+                color_dict["blue"],
+                2,
+            )  # horizontal
+            cv2.putText(
+                frame,
+                text=f"Horizontal Error: {x_diff}",
+                org=(3, FRAME_HEIGHT - 3),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.75,
+                color=color_dict["blue"],
+                thickness=2,
+            )
+            cv2.putText(
+                frame,
+                text=f"Vertical Error: {y_diff}",
+                org=(3, FRAME_HEIGHT - 18),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.75,
+                color=color_dict["blue"],
+                thickness=2,
+            )
+        else:
+            # reset bounding box
+            bbox = (0, 0, 0, 0)  # x, y, w, h
+
+            # reset center of object
+            mid_x, mid_y = 0, 0
 
     # instruction
-    cv2.putText(frame,
-                text="Press k to select object to track!",
-                org=(3, 15),
-                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                fontScale=0.5,
-                color=color_dict["yellow"],
-                thickness=1)
-    cv2.putText(frame,
-                text="Press q to exit!",
-                org=(3, 30),
-                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                fontScale=0.5,
-                color=color_dict["yellow"],
-                thickness=1)
-    
+    cv2.putText(
+        frame,
+        text="Press k to select object to track!",
+        org=(3, 15),
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        fontScale=0.5,
+        color=color_dict["yellow"],
+        thickness=1,
+    )
+    cv2.putText(
+        frame,
+        text="Press q to exit!",
+        org=(3, 30),
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        fontScale=0.5,
+        color=color_dict["yellow"],
+        thickness=1,
+    )
+
     # draw frame crosshair
-    cv2.line(frame, (mid_frame[0], FRAME_HEIGHT // 3), (mid_frame[0], FRAME_HEIGHT - FRAME_HEIGHT // 3),
-             color_dict["green"], 2)  # vertical
-    cv2.line(frame, (FRAME_WIDTH // 3, mid_frame[1]), (FRAME_WIDTH - FRAME_WIDTH // 3, mid_frame[1]),
-             color_dict["green"], 2)  # horizontal
+    cv2.line(
+        frame,
+        (mid_frame[0], FRAME_HEIGHT // 3),
+        (mid_frame[0], FRAME_HEIGHT - FRAME_HEIGHT // 3),
+        color_dict["green"],
+        2,
+    )  # vertical
+    cv2.line(
+        frame,
+        (FRAME_WIDTH // 3, mid_frame[1]),
+        (FRAME_WIDTH - FRAME_WIDTH // 3, mid_frame[1]),
+        color_dict["green"],
+        2,
+    )  # horizontal
     cv2.imshow("Webcam Feed", frame)
 
 cap.release()
--- scripts/test.py
+++ scripts/test.py
@@ -1,2 +1,31 @@
+import serial
 import sys
-print(sys.platform)
+import time
+arduino = serial.Serial(port='/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_7583033343935150A092-if00', 
+                        baudrate=115200,
+                        timeout=0,
+                        )
+print(arduino.name)
+i = 0
+while i < 10:
+    time.sleep(2.0)
+    arduino.write('hello'.encode('utf-8'))
+    i += 1
+arduino.close()
+# def write_read(x):
+#     arduino.write(bytes(x, 'utf-8'))
+#     time.sleep(0.5)
+#     data = arduino.readline()
+#     return data
+# # x, y = -1, 2
+# # print(f'{x}')
+# # print(arduino.write(bytes(f'{x}', 'utf-8')))
+# # data = arduino.readline()
+# # print(data)
+# i = 0
+# while True:
+#     # arduino.flush()
+
+#     d = write_read(f'{i}')
+#     print(d)
+#     i += 1
